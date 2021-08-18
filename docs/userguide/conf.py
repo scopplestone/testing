d6fd6ad890022c8f858a3ca7fd299910d915663a
Stephen Copplestone <stephen@copplestone.de>
2021-08-18 16:14:57 +0200

    up

--- docs/userguide/conf.py
+++ docs/userguide/conf.py
@@ -33,7 +33,8 @@ extensions = [
         'sphinx.ext.autosectionlabel',
         'sphinxcontrib.bibtex',
         'sphinx_rtd_theme',
-        'myst_parser']
+        'myst_parser'
+        ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
